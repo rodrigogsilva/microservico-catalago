65183a575fa6798b35d67d035e40e2c46982d57b
Rodrigo Gonçalves <rodrigogoncalveess@gmail.com~>
2021-02-24 04:21:46 +0000

    criação gênero

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
-    use SoftDeletes, \App\Models\Traits\Uuid;
+    use SoftDeletes, Traits\Uuid;
 
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     public $incrementing = false;
     protected $keyType = 'string';
